18a77eba26bd676e70a19947ad3d26ef1e1a7d64
Gabriel Britain <quontas@gmail.com>
2019-04-24 20:50:28 -0500

    Changed SQLite to mailchimp

--- src/routes/email.ts
+++ src/routes/email.ts
@@ -1,42 +1,22 @@
 import { Request, Response } from "express";
-import { Email, sequelize } from "../models";
-import { MailOptions } from "nodemailer/lib/stream-transport";
-import { emailController } from "../controllers/email";
-
-const EMAIL_REGEX = /^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+.)?[a-zA-Z]+.)?(tamu|email.tamu|neo.tamu).edu$/g;
-
-const mailOptions: MailOptions = {
-  from: "The TAMUhack Team <hello@tamuhack.com>",
-  to: "",
-  subject: "Howdy!",
-  text: "Body Text",
-  html: "<b>BODY TEXT</b>"
-};
-
-const emailIsValid = (email: string): boolean => {
-  return EMAIL_REGEX.test(email);
-};
-
-const respondWithError = (res: Response, errMsg: string) => {
-  res.json({
-    error: errMsg
-  });
-};
+import { emailController, SubscriptionStatus } from "../controllers/email";
+import { RequestResponse } from "request";
+import { StatusCodeError } from "request-promise/errors";
 
 export const registerEmail = async (req: Request, res: Response) => {
-  await sequelize.sync();
   const { email } = req.body;
-  if (emailIsValid(email)) {
-    const count = await Email.count({ where: { email } });
-    if (count < 1) {
-      await Email.create({ email });
-      mailOptions.to = email;
-      await emailController.sendEmail(mailOptions);
-      res.sendStatus(200);
-    } else {
-      respondWithError(res, "You've already joined our mailing list!");
+  emailController.checkIfContactSubscribed(email).then(isSubscribed => {
+    if (!isSubscribed) {
+      emailController
+        .addContactToList(email, SubscriptionStatus.Subscribed)
+        .then((response: RequestResponse) => {
+          res.sendStatus(200);
+        })
+        .catch(err => {
+          if (err instanceof StatusCodeError) {
+            res.json({ error: JSON.parse((err.response as any).body).title });
+          }
+        });
     }
-  } else {
-    respondWithError(res, "We only accept tamu.edu domains.");
-  }
+  });
 };
