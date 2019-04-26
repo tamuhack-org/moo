a2a7413281f6a1072b38498208d36a569f2e916e
Gabriel Britain <quontas@gmail.com>
2019-04-25 20:17:42 -0500

    Moved to AppEngine! No more weird hacks needed.

--- src/routes/email.ts
+++ src/routes/email.ts
@@ -1,22 +1,14 @@
 import { Request, Response } from "express";
-import { emailController, SubscriptionStatus } from "../controllers/email";
-import { RequestResponse } from "request";
-import { StatusCodeError } from "request-promise/errors";
+import { dbController } from "../controllers/db";
 
-export const registerEmail = async (req: Request, res: Response) => {
+export const emailRoute = async (req: Request, res: Response) => {
   const { email } = req.body;
-  emailController.checkIfContactSubscribed(email).then(isSubscribed => {
-    if (!isSubscribed) {
-      emailController
-        .addContactToList(email, SubscriptionStatus.Subscribed)
-        .then((response: RequestResponse) => {
-          res.sendStatus(200);
-        })
-        .catch(err => {
-          if (err instanceof StatusCodeError) {
-            res.json({ error: JSON.parse((err.response as any).body).title });
-          }
-        });
-    }
-  });
+  dbController
+    .insertEmail(email)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 };
