6ffd6544d1086c54f721bf8cdcd1a0914e3bf6fa
Gabriel Britain <quontas@gmail.com>
2019-05-12 22:01:16 -0700

    Added email functionality to Howdy Hack.

--- src/routes/hh.ts
+++ src/routes/hh.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { join } from "path";
 
 export const howdyHackRoute = async (req: Request, res: Response) => {
   res.render("hh-landing.html");
