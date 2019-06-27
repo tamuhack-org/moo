e146c050e3a3c051fdbe848887370d9919861fdd
Anthony <yihonganthony@gmail.com>
2019-06-27 18:34:14 -0500

    Added Aman's code to this updated branch

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 import { join } from "path";
 
 export const hubRoute = async (req: Request, res: Response) => {
-  res.render("index.html");
+  res.render("workshop.html");
 };
