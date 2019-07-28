9e9dde4a68bae036403baac7301bdbd596646379
Jay Khatri <khatri.florida@gmail.com>
2019-07-28 10:45:20 +0200

    init

--- src/routes/workshop.ts
+++ src/routes/workshop.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express";
-import { eventbriteController } from "../controllers/eventbrite";
+import { eventbriteController, EventbriteEvent } from "../controllers/eventbrite";
 
 export const workshopRoute = async (req: Request, res: Response) => {
-  const events = await eventbriteController.fetchEvents();
-  res.render("workshop.hbs", { events, cache: true });
+  let events;
+  try {
+    events = await eventbriteController.fetchEvents();
+  }
+  catch (err) {
+    console.error("Error fetching events", err);
+  }
+  finally {
+    res.render("workshop.hbs", { events, cache: true });
+  }
 };
 
 
