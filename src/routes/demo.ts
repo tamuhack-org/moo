68739a636a67690fb5beff9b55ce13d55ccd51d5
jonathan-innis <jonathan.innis.ji@gmail.com>
2019-09-06 10:27:35 -0500

    Fixed a dumb error

--- src/routes/demo.ts
+++ src/routes/demo.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-const DEVPOST_LINK = "howdyhack2019.devpost.com"
+const DEVPOST_LINK = "https://howdyhack2019.devpost.com"
 
 export const demoRoute = async (req: Request, res: Response) => {
     res.redirect(DEVPOST_LINK);
