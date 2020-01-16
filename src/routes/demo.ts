8d3cff89b94243f3f135a469c4d911c0aba5c34b
jonathan-innis <jonathan.innis.ji@gmail.com>
2020-01-15 22:00:19 -0600

    Added all the links

--- src/routes/demo.ts
+++ src/routes/demo.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from 'express';
 
-const DEVPOST_LINK = 'https://howdyhack2019.devpost.com';
+const DEVPOST_LINK = 'http://tamuhack2020.devpost.com/';
 
 export const demoRoute = async (req: Request, res: Response) => {
   res.redirect(DEVPOST_LINK);
--- src/routes/facebook.ts
+++ src/routes/facebook.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from 'express';
 
-const FACEBOOK_LINK = 'https://www.facebook.com/groups/2606547856036002/';
+const FACEBOOK_LINK = 'https://www.facebook.com/groups/1173997689655556/?source_id=452096011588482';
 
 export const facebookRoute = async (req: Request, res: Response) => {
   res.redirect(FACEBOOK_LINK);
