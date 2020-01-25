1ac36682ebee67c1f987e1039e9cdc2b2193acc9
jonathan-innis <jonathan.innis.ji@gmail.com>
2020-01-25 10:59:38 -0600

    Changed slack to invite link

--- src/routes/slack.ts
+++ src/routes/slack.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from 'express';
 
 const SLACK_URL =
-    'http://tamuhack2020.slack.com/';
+    'https://join.slack.com/t/tamuhack2020/shared_invite/enQtODA0Njk4MzU3MDEwLWExZTdkODQyNmEyMzQ1ZWRiMGRjM2JkNjNlZDQxYTEwNzEzZWJjMTYwMjY5Y2ExZTJkNDcyNTdhYzE3ZGQ3Yzc';
 
 export const slackRoute = async (req: Request, res: Response) => {
   res.redirect(SLACK_URL);
