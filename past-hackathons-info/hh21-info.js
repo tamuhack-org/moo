6b587603d3b6d67fe6e22872a84953f0a25c197e
Justin Le <justinle2006@gmail.com>
2025-03-12 00:45:17 -0500

    midway checkpoint to make sure that things are going as intended

---
+++ past-hackathons-info/hh21-info.js
@@ -0,0 +1,161 @@
+const hh21info = {
+    name: "HowdyHack 2021",
+    link: "/hh/2021",
+    logo: "hh21",
+    description: "Our first hybrid Hackathon!",
+    directors: [
+        {
+            webp: "./static/assets/headshots/compressed/anthony.webp",
+            img: "./static/assets/headshots/compressed/anthony.jpg",
+            name: "Anthony Teo",
+            position: "President",
+            github: "https://github.com/eightants",
+            linkedin: "https://www.linkedin.com/in/anthonyteo"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/maggieP.webp",
+            img: "./static/assets/headshots/compressed/maggieP.jpg",
+            name: "Meghana Pothugunta",
+            position: "President",
+            linkedin: "https://www.linkedin.com/in/meghana-pothugunta-7161a8183"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/aryan.webp",
+            img: "./static/assets/headshots/compressed/aryan.jpg",
+            name: "Aryan Shetty",
+            position: "Logistics Lead",
+            github: "https://github.com/aryanshetty711",
+            linkedin: "https://www.linkedin.com/in/aryan-shetty-860717148"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/vivian.webp",
+            img: "./static/assets/headshots/compressed/vivian.jpg",
+            name: "Vivian Yu",
+            position: "Design Lead",
+            github: "https://github.com/vivians-y",
+            linkedin: "https://www.linkedin.com/in/vivian-yu-aa1443194"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/nathan.webp",
+            img: "./static/assets/headshots/compressed/nathan.jpg",
+            name: "Nathan Reddy",
+            position: "Tech Lead",
+            github: "https://github.com/onewebdev",
+            linkedin: "https://www.linkedin.com/in/nathan-r-profile"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/cameronB2.webp",
+            img: "./static/assets/headshots/compressed/cameronB2.jpg",
+            name: "Cameron Brill",
+            position: "Tech Mentor",
+            github: "https://github.com/cameronbrill",
+            linkedin: "https://www.linkedin.com/in/cameronbrill"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/nathanR.webp",
+            img: "./static/assets/headshots/compressed/nathanR.jpg",
+            name: "Nathan Read",
+            position: "Logistics",
+            github: "https://github.com/nathancread",
+            linkedin: "https://www.linkedin.com/in/nathancread"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/adil.webp",
+            img: "./static/assets/headshots/compressed/adil.jpg",
+            name: "Adil Rasiyani",
+            position: "Tech",
+            github: "https://github.com/AdilRas",
+            linkedin: "https://www.linkedin.com/in/adil-rasiyani"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/dylan.webp",
+            img: "./static/assets/headshots/compressed/dylan.jpg",
+            name: "Dylan Theriot",
+            position: "Tech",
+            github: "https://github.com/dylantheriot",
+            linkedin: "https://www.linkedin.com/in/dylan-theriot"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/yashas.webp",
+            img: "./static/assets/headshots/compressed/yashas.jpg",
+            name: "Yashas Salankimatt",
+            position: "Hardware",
+            github: "https://github.com/yashas-salankimatt",
+            linkedin: "https://www.linkedin.com/in/yashas-s"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/joanna.webp",
+            img: "./static/assets/headshots/compressed/joanna.jpg",
+            name: "Joanna Zhou",
+            position: "Logistics",
+            github: "https://github.com/jo-zho",
+            linkedin: "https://www.linkedin.com/in/joannazhouyf/"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/anikait.webp",
+            img: "./static/assets/headshots/compressed/anikait.jpg",
+            name: "Anikait Sharma",
+            position: "Tech & Logistics",
+            github: "https://github.com/anikait627",
+            linkedin: "https://www.linkedin.com/in/anikait"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/rohit.webp",
+            img: "./static/assets/headshots/compressed/rohit.jpg",
+            name: "Rohit Barichello",
+            position: "Logistics",
+            github: "https://github.com/rohitbarichello",
+            linkedin: "https://www.linkedin.com/in/rohitbarichello"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/sabrina.webp",
+            img: "./static/assets/headshots/compressed/sabrina.jpg",
+            name: "Sabrina Lu",
+            position: "Logistics",
+            github: "https://github.com/sabrinalu999",
+            linkedin: "https://www.linkedin.com/in/sabrinamjlu"
+        },
+        {
+            webp: "./static/assets/headshots/compressed/ricky.webp",
+            img: "./static/assets/headshots/compressed/ricky.jpg",
+            name: "Ricky Bengani",
+            position: "Design",
+            linkedin: "https://www.linkedin.com/in/rickybengani"
+        },
+        {
+            webp: "./static/assets/headshots/new/Abhishek.jpg",
+            img: "./static/assets/headshots/new/Abhishek.jpg",
+            name: "Abhishek More",
+            position: "Tech",
+            linkedin: "https://www.linkedin.com/in/abhishek-more-linked/"
+        },
+        {
+            webp: "./static/assets/headshots/new/Jennifer.jpg",
+            img: "./static/assets/headshots/new/Jennifer.jpg",
+            name: "Jennifer Yuan",
+            position: "Design",
+            linkedin: "https://www.linkedin.com/in/jennifer-yuan-tamu/"
+        },
+        {
+            webp: "./static/assets/headshots/new/Masabarakiza.jpg",
+            img: "./static/assets/headshots/new/Masabarakiza.jpg",
+            name: "Irené Masabarakiza",
+            position: "Logistics",
+            linkedin: "https://www.linkedin.com/in/irené-masabarakiza-57543319b"
+        },
+        {
+            webp: "./static/assets/headshots/new/Malavi.jpg",
+            img: "./static/assets/headshots/new/Malavi.jpg",
+            name: "Malavi Srinivasan",
+            position: "Tech",
+            linkedin: "https://www.linkedin.com/in/malavi-srinivasan-9452a21ba/"
+        },
+        {
+            webp: "./static/assets/headshots/new/Ekdev.jpg",
+            img: "./static/assets/headshots/new/Ekdev.jpg",
+            name: "Ekdev Rajkitkul",
+            position: "Logistics",
+            linkedin: "https://www.linkedin.com/in/ekdevrajkitkul"
+        }
+    ]
+}
---
+++ past-hackathons-info/th22-info.js
@@ -0,0 +1,221 @@
+const th22info = {
+    name: "TAMUhack 2022",
+    link: "/th/2022",
+    logo: "th22",
+    description: "The 2022 installment of your favorite hackathon!",
+    directors: [
+        {
+            webp: "./static/th-2022/headshots/webp/anthony.webp",
+            img: "./static/th-2022/headshots/jpg/anthony.jpg",
+            name: "Anthony Teo",
+            position: "President",
+            github: "https://github.com/eightants",
+            linkedin: "https://www.linkedin.com/in/anthonyteo"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/meghana.webp",
+            img: "./static/th-2022/headshots/jpg/meghana.jpg",
+            name: "Meghana Pothugunta",
+            position: "President",
+            linkedin: "https://www.linkedin.com/in/meghana-pothugunta-7161a8183"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/aryan.webp",
+            img: "./static/th-2022/headshots/jpg/aryan.jpg",
+            name: "Aryan Shetty",
+            position: "Logistics Lead",
+            github: "https://github.com/aryanshetty711",
+            linkedin: "https://www.linkedin.com/in/aryan-shetty-860717148"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/vivian.webp",
+            img: "./static/th-2022/headshots/jpg/vivian.jpg",
+            name: "Vivian Yu",
+            position: "Design Lead",
+            github: "https://github.com/vivians-y",
+            linkedin: "https://www.linkedin.com/in/vivian-yu-aa1443194"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/nathan_reddy.webp",
+            img: "./static/th-2022/headshots/jpg/nathan_reddy.jpg",
+            name: "Nathan Reddy",
+            position: "Tech Lead",
+            github: "https://github.com/onewebdev",
+            linkedin: "https://www.linkedin.com/in/nathan-r-profile"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/nathan_read.webp",
+            img: "./static/th-2022/headshots/jpg/nathan_read.jpg",
+            name: "Nathan Read",
+            position: "Logistics",
+            github: "https://github.com/nathancread",
+            linkedin: "https://www.linkedin.com/in/nathancread"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/adil.webp",
+            img: "./static/th-2022/headshots/jpg/adil.jpg",
+            name: "Adil Rasiyani",
+            position: "Tech",
+            github: "https://github.com/AdilRas",
+            linkedin: "https://www.linkedin.com/in/adil-rasiyani"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/dylan.webp",
+            img: "./static/th-2022/headshots/jpg/dylan.jpg",
+            name: "Dylan Theriot",
+            position: "Tech",
+            github: "https://github.com/dylantheriot",
+            linkedin: "https://www.linkedin.com/in/dylan-theriot"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/yashas.webp",
+            img: "./static/th-2022/headshots/jpg/yashas.jpg",
+            name: "Yashas Salankimatt",
+            position: "Hardware",
+            github: "https://github.com/yashas-salankimatt",
+            linkedin: "https://www.linkedin.com/in/yashas-s"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/joanna.webp",
+            img: "./static/th-2022/headshots/jpg/joanna.jpg",
+            name: "Joanna Zhou",
+            position: "Logistics",
+            github: "https://github.com/jo-zho",
+            linkedin: "https://www.linkedin.com/in/joannazhouyf/"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/anikait.webp",
+            img: "./static/th-2022/headshots/jpg/anikait.jpg",
+            name: "Anikait Sharma",
+            position: "Tech & Logistics",
+            github: "https://github.com/anikait627",
+            linkedin: "https://www.linkedin.com/in/anikait"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/rohit.webp",
+            img: "./static/th-2022/headshots/jpg/rohit.jpg",
+            name: "Rohit Barichello",
+            position: "Logistics",
+            github: "https://github.com/rohitbarichello",
+            linkedin: "https://www.linkedin.com/in/rohitbarichello"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/sabrina.webp",
+            img: "./static/th-2022/headshots/jpg/sabrina.jpg",
+            name: "Sabrina Lu",
+            position: "Logistics",
+            github: "https://github.com/sabrinalu999",
+            linkedin: "https://www.linkedin.com/in/sabrinamjlu"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/ricky.webp",
+            img: "./static/th-2022/headshots/jpg/ricky.jpg",
+            name: "Ricky Bengani",
+            position: "Design",
+            github: "https://github.com/rickybengani",
+            linkedin: "https://www.linkedin.com/in/rickybengani"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/abhishek.webp",
+            img: "./static/th-2022/headshots/jpg/abhishek.jpg",
+            name: "Abhishek More",
+            position: "Tech",
+            linkedin: "https://www.linkedin.com/in/abhishek-more-linked/"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/jennifer.webp",
+            img: "./static/th-2022/headshots/jpg/jennifer.jpg",
+            name: "Jennifer Yuan",
+            position: "Design",
+            linkedin: "https://www.linkedin.com/in/jennifer-yuan-tamu/"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/irene.webp",
+            img: "./static/th-2022/headshots/jpg/irene.jpg",
+            name: "Irené Masabarakiza",
+            position: "Logistics",
+            linkedin: "https://www.linkedin.com/in/irené-masabarakiza-57543319b"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/malavi.webp",
+            img: "./static/th-2022/headshots/jpg/malavi.jpg",
+            name: "Malavi Srinivasan",
+            position: "Design",
+            linkedin: "https://www.linkedin.com/in/malavi-srinivasan-9452a21ba/"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/ekdev.webp",
+            img: "./static/th-2022/headshots/jpg/ekdev.jpg",
+            name: "Ekdev Rajkitkul",
+            position: "Logistics",
+            linkedin: "https://www.linkedin.com/in/ekdevrajkitkul"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/jiyoon.webp",
+            img: "./static/th-2022/headshots/jpg/jiyoon.jpg",
+            name: "Jiyoon Kim",
+            position: "Logistics",
+            linkedin: "https://www.linkedin.com/in/jiyoon-kim1"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/jaden.webp",
+            img: "./static/th-2022/headshots/jpg/jaden.jpg",
+            name: "Jaden Banze",
+            position: "Tech & Design",
+            github: "https://github.com/jadenbanze",
+            linkedin: "https://www.linkedin.com/in/jadenbanze/"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/naveen.webp",
+            img: "./static/th-2022/headshots/jpg/naveen.jpg",
+            name: "Naveen Iyer",
+            position: "Tech",
+            github: "https://github.com/NitroGuy10",
+            linkedin: "https://www.linkedin.com/in/naveen-iyer-66412b225/"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/kirthivel.webp",
+            img: "./static/th-2022/headshots/jpg/kirthivel.jpg",
+            name: "Kirthivel Ramesh",
+            position: "Design",
+            linkedin: "https://www.linkedin.com/in/kirthivel"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/ayomide.webp",
+            img: "./static/th-2022/headshots/jpg/ayomide.jpg",
+            name: "Ayomide Fatoye",
+            position: "Logistics",
+            github: "https://github.com/Ayo-Fatoye",
+            linkedin: "https://www.linkedin.com/in/ayomide-fatoye-b6a0751b8/"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/ashrita.webp",
+            img: "./static/th-2022/headshots/jpg/ashrita.jpg",
+            name: "Ashrita Vadlapatla",
+            position: "Design"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/arya.webp",
+            img: "./static/th-2022/headshots/jpg/arya.jpg",
+            name: "Arya Gupta",
+            position: "Logistics",
+            linkedin: "https://www.linkedin.com/in/aryagupta12345"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/van.webp",
+            img: "./static/th-2022/headshots/jpg/van.jpg",
+            name: "Vanessa Le",
+            position: "Tech",
+            github: "https://github.com/mopositive2001",
+            linkedin: "http://linkedin.com/in/vanessa-le-a82335199"
+        },
+        {
+            webp: "./static/th-2022/headshots/webp/anish.webp",
+            img: "./static/th-2022/headshots/jpg/anish.jpg",
+            name: "Anish Karthik",
+            position: "Logistics",
+            github: "https://github.com/anishfish2",
+            linkedin: "https://www.linkedin.com/in/anishkarthik"
+        }
+    ]
+}
