6419014964149b8c529a3eb162b5c1dbcfcf054a
Justin Le <justinle2006@gmail.com>
2025-03-12 00:59:40 -0500

    made all timeline into separate files for better organization

---
+++ past-hackathons-info/th18-info.js
@@ -0,0 +1,6 @@
+const th18info = {
+    name: "TAMUhack 2018",
+    link: "/th/2018",
+    logo: "th18",
+    description: "Even the best traditions need a little change. We switched from a Fall hackathon to a Spring hackathon."
+}
