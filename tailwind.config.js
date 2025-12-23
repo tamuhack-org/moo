151b57d1d9a5b3e3ed61ba04ead1de50a3506153
Ju5t1nL3 <justinle2006@gmail.com>
2025-12-22 21:31:56 -0600

    Autoformat with prettier and fix syntax errors
    When running prettier, there were a ton of syntax errors (largely
    mismatching tags, like an extra </div> or tags that looked like
    <script></SCRIPT>). I simply fixed all these errors until npm run format
    ran with no errors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,22 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
-      extend: {
-        transitionDuration: {
-         '0': '0ms',
-         '2000': '2000ms',
+        extend: {
+            transitionDuration: {
+                0: '0ms',
+                2000: '2000ms',
+            },
+            keyframes: {
+                wiggle: {
+                    '0%, 100%': { transform: 'rotate(-30deg)' },
+                    '50%': { transform: 'rotate(30deg)' },
+                },
+            },
+            animation: {
+                wiggle: 'wiggle 1s ease-in-out infinite',
+            },
         },
-        keyframes: {
-          wiggle: {
-            '0%, 100%': { transform: 'rotate(-30deg)' },
-            '50%': { transform: 'rotate(30deg)' },
-          }
-         },
-         animation: {
-          wiggle: 'wiggle 1s ease-in-out infinite',
-         }
-      },
     },
     variants: {},
     plugins: [],
-}
+};
