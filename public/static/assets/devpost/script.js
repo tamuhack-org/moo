0b5f2d4873537533e1ad2a4b140532c52666fc88
Eric Li <eric.li.8736@gmail.com>
2026-07-14 15:49:45 -0700

    Adjust scroll margins on how-to-devpost page

--- public/static/assets/devpost/script.js
+++ public/static/assets/devpost/script.js
@@ -1,20 +1,21 @@
-const changeNav = (entries, observer) => {
+const changeNav = (entries) => {
 	entries.forEach((entry) => {
 		if(entry.isIntersecting && entry.intersectionRatio >= 0.85) {
 			document.querySelector('.link-active').classList.remove('link-active');
-			const id = entry.target.getAttribute('id');
-			const newLink = document.querySelector(`[href="#${id}"]`).parentElement.classList.add('link-active');
+			const id = entry.target.parentElement.getAttribute('id');
+			document.querySelector(`[href="#${id}"]`).parentElement.classList.add('link-active');
 		}
 	});
 }
 
 const options = {
-	threshold: 0.85
+	threshold: 1,
+	rootMargin: "0px 0px -70% 0px"
 }
 
 const observer = new IntersectionObserver(changeNav, options);
 
-const sections = document.querySelectorAll('section');
+const sections = document.querySelectorAll('h2');
 sections.forEach((section) => {
 	observer.observe(section);
 });
